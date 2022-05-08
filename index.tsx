3f59f5ad6ddeb016ebce190f4c12aa09e890681e
enchanter5525 <bhanun50@gmail.com>
2022-05-08 15:46:46 +0530

    2 files modified

--- index.tsx
+++ index.tsx
@@ -15,27 +15,30 @@ class App extends Component<AppProps, AppState> {
       name: 'React'
     };
   }
+// Expandable components from instagram
 
   render() {
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
+     <div className="expandable-container">
+       <input type='checkbox' id="check-input" className="check-input"/>
+       <label htmlFor="check-input" className="expandable">
+        <header className="header-container">
+          <div className="header">
+            <h4 className="title">Expandable Widget</h4>
+            <span className="subtitle">HTML and CSS</span>
+
+          </div>
+          <span className="icon"></span>
+        </header>
+        <p className="content">
+          lorem ipsum df fdjhfjdf dfhdhfm fdhjfdnf fdfkdf dfdf fdnfkdjfkd dsfhdjfldshfldshfldshilfufg;ahglfdughlfgh;hg;girh gr girgirg g righ ir gir girgirhgi r girg irg ior griog irg iorg ior gior gjior goi giro ghuirghuiaggr;h g;re gr g
+          gr grigirg ior grg
+          rg rr;gr'
+          
         </p>
-        <div className="parent">
-          <div className="child">
+       </label>
 
-          </div>
-      </div>
-      <hr/>
-
-      <div className="parent2">
-          <div className="child2">
-
-          </div>
-      </div>
-      </div>
+     </div>
     );
   }
 }
