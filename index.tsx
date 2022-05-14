08d6fc3bcc6e797303535544d10ec58340e080a2
enchanter5525 <bhanun50@gmail.com>
2022-05-14 15:10:08 +0530

    2 files modified

--- index.tsx
+++ index.tsx
@@ -2,8 +2,8 @@ import React, { Component, useState } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.scss';
-// import DatePicker from 'react-date-picker';
-import DatePicker from 'react-date-picker/dist/entry.nostyle';
+import DatePicker from 'react-date-picker';
+// import DatePicker from 'react-date-picker/dist/entry.nostyle';
 interface AppProps {}
 interface AppState {
   name: string;
